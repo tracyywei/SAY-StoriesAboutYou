b1a37441687d0df001df582f1ae529d71b198d6c
Tracy Wei <tracywei@lihongs-macbook-pro.local>
2018-06-04 22:43:32 -0500

    Initial Commit

---
+++ SAY Houston/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SAY Houston
+//
+//  Created by Tracy Wei on 6/4/18.
+//  Copyright © 2018 Game Changers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
